0e6d93c67541370f88dad760f7eaa1eb2519116c
Julian Puerto <102914592+jpuerto1604@users.noreply.github.com>
2024-10-12 22:08:38 -0500

    Data replaes

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import os
 
-
 df1 = pd.DataFrame(np.array([[0,6,8,10,12],[12,0,6,8,10],[10,6,0,6,8],[8,8,6,0,6],[6,10,8,6,0]]),columns=["LU","M1","M2",'M3',"M4"],index=["LU","M1","M2",'M3',"M4"])
 df2 = pd.DataFrame(np.array([[0,4,6,8,6],[6,0,2,4,2],[8,12,0,2,4],[6,10,12,0,2],[4,8,10,12,0]]),columns=["LU","M1","M2",'M3',"M4"],index=["LU","M1","M2",'M3',"M4"])
 df3 = pd.DataFrame(np.array([[0,2,4,10,12],[12,0,2,8,10],[10,12,0,6,8],[4,6,8,0,2],[2,4,6,12,0]]),columns=["LU","M1","M2",'M3',"M4"],index=["LU","M1","M2",'M3',"M4"])
@@ -19,4 +18,4 @@ def t_times(layout,start,end):
         case 4:
             return df4.loc[start,end]
 
-df=pd.read_excel('/Users/julian/Documentos/Thesis/Instances Data.xlsm',sheet_name='Macrodata',usecols=['F:H'])
+xls = pd.read_excel('/Users/julian/Documentos/Thesis/Data.xlsx',sheet_name='Macrodata', usecols='F:H')
